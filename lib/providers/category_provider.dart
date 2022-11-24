7549ad06d431ef22720d899dd11914d73dce8837
Beyza Uyanıksoy <beyzauyanik57@hotmail.com>
2022-11-24 21:24:32 +0300

    home page sayfasında değişiklikler yapıldı ve playlist model oluşturuldu

--- lib/providers/category_provider.dart
+++ lib/providers/category_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../model/featured_playlists_model.dart';
 import '../model/spotify_model.dart';
 import '../services/spotify_services.dart';
 
@@ -7,6 +8,13 @@ class SpotifyProvider with ChangeNotifier {
   SpotifyCategoryModel? response = SpotifyCategoryModel();
   getCategory() async {
     response = await Services().getSpotifyModel();
+
+    notifyListeners();
+  }
+
+  FeaturedPlaylistsModel? responseplaylist = FeaturedPlaylistsModel();
+  getPlayList() async {
+    responseplaylist = await Services().getPlayList();
 
     notifyListeners();
   }
--- lib/services/spotify_services.dart
+++ lib/services/spotify_services.dart
@@ -1,19 +1,21 @@
 import 'package:dio/dio.dart';
 
+import '../model/featured_playlists_model.dart';
 import '../model/spotify_model.dart';
 
 class Services {
-  final Dio _dio =
-      Dio(BaseOptions(baseUrl: "https://api.spotify.com/v1/", headers: {
-    "Authorization":
-        "Bearer BQCtje8UL9a__dXZkQ7FtI6z8JHS-vwTykrmei-XK6OZ9qXNcmLjqt44a332q1sbyVk30U4YwldfTdp6t3lhdEbDmdMYteqqtuG3gWxvf4HwG-h-80bE8DPefs6lPo3KvXS1iQnGt6UKS9_JnJ3dPoyaCzSEdazJrvZWuYypEwQ6sCjHX5q0opPWNQpg2y6Qd-Na6bcOq4L1iG8Q7zncZCV4-SEva5Ddx0sAs6ODuFovL2SlVY7UrIvV0sT0OeK93T6EmEEmWuwpJfBH_Naqu0Br4fAIfJ1czXct6tIc"
-  }));
+  final Dio _dio = Dio(
+    BaseOptions(baseUrl: "https://api.spotify.com/v1/", headers: {
+      "Authorization":
+          "Bearer BQB7WQ8bTNBb3C8POu2L9pZvNk8-x2QJeN4jpVAYwlKwvulJvxsBM5wwORooOSi192bidnistNmFxiJR5M-v7flNk8UUregwlJqF5Mmc_X_DFv6JCk4X5bI4wSwrP6-x08zSukF3xz7seT9ksp445TiYBJSBhoup58CmZ6heTJoa5GaRhy2Y_b6xw_ImWKKOKBq0vmfnJ_ZDhbttlQ0n4UhJmo8hljcF8lPRzZftEYLK45LqwtIHUpoxClgvYEYkfcsa5uOw6XqvsEc-dJvZhgXUSPxA9o8Quno5vdnR"
+    }),
+  );
 
   Future<SpotifyCategoryModel?> getSpotifyModel() async {
     SpotifyCategoryModel? searchModel;
     try {
-      final response = await _dio
-          .get("browse/categories?country=TR&locale=tr_TR&limit=20&offset=1");
+      final response =
+          await _dio.get("browse/featured-playlists?country=TR&locale=tr_TR");
 
       if (response.statusCode == 200) {
         print("Çalışıyor");
@@ -23,6 +25,26 @@ class Services {
         print("Çalışmıyor");
       }
       return searchModel;
+    } catch (e) {
+      print(e);
+    }
+    return null;
+  }
+
+  Future<FeaturedPlaylistsModel?> getPlayList() async {
+    FeaturedPlaylistsModel? playlistmodel;
+    try {
+      final response1 = await _dio
+          .get("browse/categories?country=TR&locale=tr_TR&limit=20&offset=1");
+
+      if (response1.statusCode == 200) {
+        print("Çalışıyor");
+        playlistmodel = FeaturedPlaylistsModel.fromJson(response1.data);
+        print(playlistmodel.playlists);
+      } else {
+        print("Çalışmıyor");
+      }
+      return playlistmodel;
     } catch (e) {
       print(e);
     }
